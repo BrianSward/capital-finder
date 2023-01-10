fd28cb81b77a3e22c99468aa01e808f9dedbd12b
Brian Sward <bsward@gmail.com>
2023-01-10 10:20:51 -0800

    touched up code, fixed inputs

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -12,22 +12,23 @@ class handler(BaseHTTPRequestHandler):
 		url_bits = parse.urlsplit(s)
 		parsed_list = parse.parse_qsl(url_bits.query)
 		new_dic = dict(parsed_list)
+
 		if 'country' in new_dic:
 			country = new_dic['country']
-			url = 'https://restcountries.com/v3.1/name/'
-			r = requests.get(url + country)
+			url = "https://restcountries.com/v3.1/name/"
+			r = requests.get(url + country.lower())
 			data = r.json()
-			capital = data[0]['capital'][0]
-			message = f"The capital of {new_dic['country']} is {capital}"
+			capital = str(data[0]['capital'][0])
+			message = f"The capital of {new_dic['country'].capitalize()} is {capital.capitalize()}"
 
 		# change back elif when working
 		elif 'capital' in new_dic:
 			capital = new_dic['capital']
-			url = 'https://restcountries.com/v3.1/capital/'
-			r = requests.get(url + capital)
+			url = "https://restcountries.com/v3.1/capital/"
+			r = requests.get(url + capital.lower())
 			data = r.json()
-			country = data[0]['name']['common']
-			message = f"{new_dic['capital']} is the capital of {country}"
+			country = str(data[0]['name']['common'])
+			message = f"{new_dic['capital'].capitalize()} is the capital of {country.capitalize()}"
 		else:
 			message = "Enter a valid country or capital name"
 
